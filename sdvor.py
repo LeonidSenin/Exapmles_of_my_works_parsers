923888a7aba5e7b6e315d2ae86ebc94f8bf37f50
l.senin <l.senin@ac.gov.ru>
2023-10-24 17:13:04 +0300

    New version sdvor.py

--- sdvor.py
+++ sdvor.py
@@ -13,14 +13,14 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('max_colwidth', 70)
 
-
 period = pd.to_datetime(datetime.now().date())  # дата
 month_str = {1: 'Январь', 2: 'Февраль', 3: 'Март', 4: 'Апрель', 5: 'Май', 6: 'Июнь', 7: 'Июль', 8: 'Август',
              9: 'Сентябрь', 10: 'Октябрь', 11: 'Ноябрь', 12: 'Декабрь'}
 
 
 def get_links(articul, product, city, city_code):
-    df = pd.DataFrame(columns=['indicator', 'subindicator_1', 'region', 'data_source', 'measure_unit', 'periodity','indicator_value'])
+    df = pd.DataFrame(columns=['indicator', 'subindicator_1', 'region', 'data_source', 'measure_unit', 'periodity',
+                               'indicator_value'])
     url = f"https://www.sdvor.com/{city_code}/search/{product}"  # ссылка на товар
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
@@ -30,30 +30,39 @@ def get_links(articul, product, city, city_code):
         soup = BeautifulSoup(site.content, "html.parser")  # разбираем код страницы на элементы
         # сопоставляет код товара и артикул
         # print(city)
-        if str(articul) == soup.find('div', {'class': 'product-code'}).text.split(':')[-1].strip():
-            try:
-                price = re.sub(r'\s+', ' ', soup.find('span', {'class': 'main'}).text).replace('₽', '').replace(' ','') # цена на товар
-            except:
-                price = re.sub(r'\s+', ' ', soup.find('div', {'class': 'price'}).text).replace('₽', '').replace(' ','')
-            measure_unit = 'руб./' + soup.find('div', {'class': 'product-buy-section'}).text.split('₽')[-1].split()[
-                0]  # единица измерения товара
-            # print(city, product, price, measure_unit)
+        # print(str(articul),soup.find('div', {'class': 'product-code'}).text.split(':')[-1].strip())
+        if soup.find('div', {'class': 'product-code'}) != None:
+            if str(articul) == soup.find('div', {'class': 'product-code'}).text.split(':')[-1].strip():
+                try:
+                    price = re.sub(r'\s+', ' ', soup.find('span', {'class': 'main'}).text).replace('₽', '').replace(' ',
+                                                                                                                    '')  # цена на товар
+                except:
+                    price = re.sub(r'\s+', ' ', soup.find('div', {'class': 'price'}).text).replace('₽', '').replace(' ',
+                                                                                                                    '')
+                measure_unit = 'руб./' + soup.find('div', {'class': 'product-buy-section'}).text.split('₽')[-1].split()[
+                    0]  # единица измерения товара
+                # print(city, product, price, measure_unit)
+                df_row = pd.DataFrame(
+                    {'indicator': ['Цена на ' + product], 'subindicator_1': [str(articul)], 'region': [city],
+                     'data_source': 'Сайт ООО Строительный двор',
+                     'measure_unit': [measure_unit], 'periodity': 'Еженедельно',
+                     'indicator_value': price})  # создаем датафрейм из одной строки
+                df = pd.concat([df, df_row])  # соединяем с общим датафремом
+                df['indicator_value'] = df['indicator_value'].astype(float)
+                global df_to_db
+                df_to_db = pd.concat([df_to_db, df], ignore_index=True)
+        else:
+            pass
 
-            df_row = pd.DataFrame(
-                {'indicator': ['Цена на ' + product], 'subindicator_1': [str(articul)],'region': [city], 'data_source': 'Сайт ООО Строительный двор',
-                 'measure_unit': [measure_unit], 'periodity': 'Еженедельно',
-                 'indicator_value': price})  # создаем датафрейм из одной строки
-            df = pd.concat([df, df_row])  # соединяем с общим датафремом
-            df['indicator_value'] = df['indicator_value'].astype(float)
-            global df_to_db
-            df_to_db = pd.concat([df_to_db, df], ignore_index=True)
     else:
         pass
         print('Странички не открылась!!!', site.status_code)
 
 
 if __name__ == '__main__':
-    df_to_db = pd.DataFrame(columns=['indicator', 'subindicator_1', 'region', 'data_source', 'measure_unit', 'periodity','indicator_value'])
+    df_to_db = pd.DataFrame(
+        columns=['indicator', 'subindicator_1', 'region', 'data_source', 'measure_unit', 'periodity',
+                 'indicator_value'])
     products = {13905: 'Щебень гранитный в МКР (фр. 5-20 мм) 1000 кг',
                 13908: 'Щебень гранитный 20/40, мкр',
                 6432: 'Песок строительный крупнозернистый намывной, 1 т МКР',
@@ -80,7 +89,7 @@ if __name__ == '__main__':
               'Г. Сургут': 'surgut', 'Г. Тобольск': 'tobolsk', 'Г. Тюмень': 'tmn', 'Г. Челябинск': 'chelyabinsk',
               'Г. Ялуторовск': 'yalutorovsk'}  # перечень городов
 
-    for articul, product in products.items(): # перебор всех материалов и городов
+    for articul, product in products.items():  # перебор всех материалов и городов
         print(articul, product)
         for city, city_code in cities.items():
             get_links(articul, product, city, city_code)
@@ -95,4 +104,3 @@ if __name__ == '__main__':
 
     print(df_to_db)
     print(df_to_db.info())
-
